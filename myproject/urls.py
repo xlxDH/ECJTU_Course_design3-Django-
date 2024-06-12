9146833fd3dd46c9149b151c225c98c2d63de8e1
DH_xlx <v847588615@hotmail.com>
2024-06-12 21:15:40 +0800

    compress

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('jobseeker/',include('jobseeker.urls')),
+    #path('',include('jobseeker.urls')),
     path('admin/', admin.site.urls),
 ]
